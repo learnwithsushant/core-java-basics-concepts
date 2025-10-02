7aabc29a191637c69b7e26c3e906ef96e2c133bc
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-02 18:08:42 +0530

    Largest odd num in array

---
+++ src/main/java/learn/core/java/concepts/TUFcode/StringOperation/LargestOddNumberInString.java
@@ -0,0 +1,33 @@
+package learn.core.java.concepts.TUFcode.StringOperation;
+
+public class LargestOddNumberInString {
+
+    public static void main(String[] args) {
+        String num = "4206";
+        System.out.println("Largest Odd Number: " + largestOddNumber(num));
+
+        num = "35427";
+        System.out.println("Largest Odd Number: " + largestOddNumber(num));
+
+        num = "52";
+        System.out.println("Largest Odd Number: " + largestOddNumber(num));
+    }
+
+    private static String largestOddNumber(String num) {
+        // Traverse from rightmost digit to left
+        for (int i = num.length() - 1; i >= 0; i--) {
+            int digit = num.charAt(i) - '0'; // convert char to int
+            if (digit % 2 != 0) {
+                // Found rightmost odd digit → substring till here is largest odd number
+                String candidate = num.substring(0, i + 1);
+
+                // Remove prefix zeros (leading zeros)
+                candidate = candidate.replaceFirst("^0+", "");
+
+                // If everything was zeros, return ""
+                return candidate.isEmpty() ? "" : candidate;
+            }
+        }
+        return ""; // No odd digit found
+    }
+}
