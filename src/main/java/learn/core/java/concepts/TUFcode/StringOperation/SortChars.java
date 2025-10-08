09f6ef92ad88b66c86f9762bace3d30a62d4d5bc
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-08 23:25:52 +0530

    partial_array Freq

--- src/main/java/learn/core/java/concepts/TUFcode/StringOperation/SortChars.java
+++ src/main/java/learn/core/java/concepts/TUFcode/StringOperation/SortChars.java
@@ -1,17 +1,44 @@
 package learn.core.java.concepts.TUFcode.StringOperation;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SortChars {
     public static void main(String[] args) {
-        String  s = "bbccddaaa";
+        /*String  s = "bbccddaaa";
 
-        char[] toChar = s.toCharArray();
+        Map<Character,Integer> freQArray = new LinkedHashMap<>();
 
-        Arrays.sort(toChar);
+        for(char c : s.toCharArray()){
+            freQArray.put(c,freQArray.getOrDefault(c,0)+1);
+        }
 
+        System.out.println(freQArray.toString());
+*/
+
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('a', 5);
+        map.put('b', 2);
+        map.put('c', 8);
+        map.put('d', 1);
+
+        // Convert entry set to list
+        List<Map.Entry<Character, Integer>> list = new ArrayList<>(map.entrySet());
+
+        // Sort list by value descending
+        Collections.sort(list, new Comparator<Map.Entry<Character, Integer>>() {
+            @Override
+            public int compare(Map.Entry<Character, Integer> e1, Map.Entry<Character, Integer> e2) {
+                return e2.getValue().compareTo(e1.getValue()); // descending
+            }
+        });
+
+        // Put into LinkedHashMap to maintain order
+        Map<Character, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<Character, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+
+        System.out.println("Sorted Map (Descending): " + sortedMap);
+    }
 
     }
-}
