a6d3fcb189ef455ed24d49fa008d5e7e3561f70a
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-12 09:33:55 +0530

    Updates Second highest frequency - Day 21 of 100 days of coding

--- src/main/java/learn/core/java/concepts/TUFcode/Hashing/SecondHighestOccuringElement.java
+++ src/main/java/learn/core/java/concepts/TUFcode/Hashing/SecondHighestOccuringElement.java
@@ -3,7 +3,7 @@ package learn.core.java.concepts.TUFcode.Hashing;
 public class SecondHighestOccuringElement {
     public static void main(String[] args) {
 
-        int[] nums = {4, 4, 5, 5, 6, 7};
+        int[] nums = {100, 200, 100, 200, 100};
 
         int maxIndex = 0;
 
@@ -20,34 +20,7 @@ public class SecondHighestOccuringElement {
 
         int maxHFC = 0, hFE = -1, maxsHFC= 0, shFE = -1;
 
-       /* for (int i = 0; i < hash.length; i++) {
-
-            int count = hash[i];
-
-           // String temp = "| i: "+ i+ " | count: "+count + " | nums["+i+"]: " + nums[i]+ " |";
-
-            if (count == 0)
-                continue;
-
-            if (count > maxHFC) {
-                maxsHF = maxHFC;
-                shFE = hFE;
-                maxHFC = count;
-                hFE = i;
-            } else if (count == maxHFC && i < hFE) {
-                hFE = i;
-                ;
-            } else if (count == maxsHF && i < shFE) {
-                shFE = i;
-
-            } else if (count > maxsHF) {
-                maxsHF = count;
-                shFE = i;
-
-            }
-        }*/
-
-        for(int i : nums){
+       for(int i : nums){
             int count = hash[i];
 
             String temp = "| i: "+ i+ " | count: "+count + " | hash["+i+"]: " + hash[i]+ " |";
@@ -59,7 +32,7 @@ public class SecondHighestOccuringElement {
                 hFE = i;
             } else if (count==maxHFC&&i<hFE) {
                 hFE = i;
-            } else if (count == maxHFC && i < shFE) {
+            } else if (count == maxsHFC && i < shFE) {
                 shFE = i;
 
             } else if (count<maxHFC && count> maxsHFC) {
