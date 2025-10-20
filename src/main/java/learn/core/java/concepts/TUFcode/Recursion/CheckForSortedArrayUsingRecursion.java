2ebe0d246168b5a89924739d5524fb87f2b83bd1
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-20 23:55:14 +0530

    Recursion - sort array - Day 29 of 100 days of coding

---
+++ src/main/java/learn/core/java/concepts/TUFcode/Recursion/CheckForSortedArrayUsingRecursion.java
@@ -0,0 +1,59 @@
+package learn.core.java.concepts.TUFcode.Recursion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class CheckForSortedArrayUsingRecursion {
+
+
+    /*public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 4, 7};
+
+        boolean isSorted = false;
+
+        if(arr.length == 1)
+            System.out.println(true);
+        else if (arr.length == 2 && arr[0]== arr[1]) {
+            System.out.println(true);
+        }else
+            for(int i=1;i<arr.length;i++){
+                if(arr[i]>arr[i-1])
+                    isSorted = true;
+                else if((arr[i]==arr[i-1])){
+                    isSorted = true;
+                }
+            }
+
+        System.out.println(isSorted);
+
+    }*/
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 4, 7};
+
+        ArrayList<Integer> integerListB = IntStream.of(arr)
+                .boxed()
+                .collect(Collectors.toCollection(ArrayList::new));
+
+        System.out.println("is array Sorted "+isSortedArray(0,1,integerListB));
+
+    }
+
+    private static boolean isSortedArray(int start, int next, ArrayList<Integer> nums){
+        if(nums.size() == 1)
+            return true;
+        else if (nums.size() == 2 && nums.get(0)==nums.get(1)) {
+            return true;
+        }else if((next==nums.size()-1)&&(nums.get(start)<nums.get(next))){
+            return true;
+
+        }else if((next<=nums.size()-1)&&(nums.get(start)>nums.get(next))){
+            return false;
+        }
+            return isSortedArray(next,next+1,nums);
+    }
+
+
+}
--- src/main/java/learn/core/java/concepts/TUFcode/Recursion/ReverseArrayUsingRecursion.java
+++ src/main/java/learn/core/java/concepts/TUFcode/Recursion/ReverseArrayUsingRecursion.java
@@ -1,4 +1,32 @@
 package learn.core.java.concepts.TUFcode.Recursion;
 
+import java.util.List;
+
 public class ReverseArrayUsingRecursion {
+    static int[] nums ;
+    public static void main(String[] args) {
+
+        nums = new int[]{1, 3, 3, 3, 5};
+
+        reverseArray(0,nums.length-1,nums);
+
+        for(int n : nums){
+            System.out.print(n);
+        }
+
+    }
+
+    private static void reverseArray(int start, int end, int[] nums){
+        // Base case
+        if (start >= end) return;
+
+        // Swap characters at left and right positions
+        int temp = nums[start];
+        nums[start]= nums[end];
+        nums[end]=temp;
+
+        // Recursive call with updated indices
+        reverseArray(start + 1, end - 1,nums);
+
+    }
 }
