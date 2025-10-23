1f13a087832c9496a51329d4cc18c8d4f98356fa
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-23 23:11:07 +0530

    Advance String Reverse - Day 32 of 100 days of coding

---
+++ src/main/java/learn/core/java/concepts/TUFcode/advancedSection/StringOperation/ReverseSentense.java
@@ -0,0 +1,39 @@
+package learn.core.java.concepts.TUFcode.advancedSection.StringOperation;
+
+public class ReverseSentense {
+
+    public static void main(String[] args) {
+        String s= "I have the fastest vector";
+
+        System.out.println(reverseIt(s));
+
+    }
+
+    private static String reverseIt(String str){
+        String[] postSplit = str.split("\\s+");
+
+        if(postSplit.length==1){
+            return postSplit[0].toString();
+        }
+
+        int start = 0, end = postSplit.length - 1;
+
+        while(start<=end){
+
+            String temp = postSplit[start];
+            postSplit[start] = postSplit[end];
+            postSplit[end] = temp;
+
+            start+=1;
+            end-=1;
+        }
+
+        String packStr = "";
+        for(String temp: postSplit){
+
+            packStr+=temp+" ";
+        }
+
+        return packStr;
+    }
+}
