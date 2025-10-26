311ba5cb55c90b4b975d094d0402e59a77bf42d8
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-10-26 22:47:57 +0530

    Selection sort - Day 35 of 100 days of coding

---
+++ src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package learn.core.java.concepts.TUFcode.advancedSection.Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] nums = {7, 4, 1, 5, 3};
+
+        nums = selectionSort(nums);
+
+        Arrays.stream(nums).forEach(System.out::println);
+    }
+    
+    private static int[] selectionSort(int[] nums){
+        int size = nums.length;
+        for(int i = 0; i<=size-2;i++){
+            int min = i;
+            for(int j = i;j<=size-1;j++){
+                
+                if(nums[j]<nums[min]){
+                    min=j;
+                }
+            }
+            int temp = nums[i];
+            nums[i] = nums[min];
+            nums[min]=temp;
+        }
+
+        return nums;
+    }
+
+
+}
