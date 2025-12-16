4038f333676c3ea57c98cb484c1bb2a20e4a4fe5
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-12-16 23:42:35 +0530

    implementation bug fixes

--- src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Arrays/ConsecutiveUsingLambda.java
+++ src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Arrays/ConsecutiveUsingLambda.java
@@ -7,19 +7,24 @@ public class ConsecutiveUsingLambda {
     public static void main(String[] args) {
         int[] nums = {1, 1, 0, 1, 1, 1, 0, 1};
 
-        int longest = Arrays.stream(
-                        Arrays.toString(nums)
-                                .replaceAll("[\\[\\],]", "")
-                                .trim()
-                                .split("0"))
-                .map(String::trim)
-                .filter(s -> !s.isEmpty())
-                .mapToInt(s -> s.length())
-                .max()
-                .orElse(0);
+        int longest = Arrays.stream(nums)
+                .mapToObj(String::valueOf)     // "1", "1", "0", ...
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString()
+                .split("0")
+                .length == 0 ? 0 :
+                Arrays.stream(
+                                Arrays.stream(nums)
+                                        .mapToObj(String::valueOf)
+                                        .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                                        .toString()
+                                        .split("0"))
+                        .mapToInt(String::length)
+                        .max()
+                        .orElse(0);
+
+
 
         System.out.println("Consecutive no. of 1's :: "+longest);
-
-        // Bug in this implementation
     }
 }
