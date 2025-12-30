07a6ed5321a2f35a39943fe20bbe7d40c26ec94c
Sushant Wadjikar <wadjikarsushant@gmail.com>
2025-12-30 20:27:08 +0530

    Refactoring code

--- src/main/java/learn/core/java/concepts/TUFcode/basicMaths/AbundantNumber.java
+++ src/main/java/learn/core/java/concepts/TUFcode/basicMaths/AbundantNumber.java
@@ -2,7 +2,15 @@ package learn.core.java.concepts.TUFcode.basicMaths;
 
 public class AbundantNumber {
     public static void main(String[] args) {
-        int num = 12, sum = 0;
+        int num = 12;
+
+        boolean isAbundant = isAbundantNumber(num);
+
+        System.out.println("is it a abundant number: "+isAbundant);
+    }
+    static boolean isAbundantNumber(int num){
+
+        int sum = 0;
 
         for(int i = 1; i<num;i++){
             if(num%i==0){
@@ -11,8 +19,9 @@ public class AbundantNumber {
 
         }
         if(sum>num){
-            System.out.println("Is Abundant");
+           return true;
         }else
-            System.out.println("Not Abundant");
+            return false;
+
     }
 }
