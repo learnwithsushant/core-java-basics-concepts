46a8c5dff52818584eccae8a73ddd5e79ad6aa78
Sushant Wadjikar <wadjikarsushant@gmail.com>
2026-01-05 21:48:50 +0530

    Unique elements in array

---
+++ src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Arrays/UniqueElementsInArray.java
@@ -0,0 +1,18 @@
+package learn.core.java.concepts.TUFcode.advancedSection.Arrays;
+
+public class UniqueElementsInArray {
+    public static void main(String[] args) {
+        int[] nums = {0,2,4,4,5,7,7,8,9,9};
+
+        int i = 0;
+
+        for(int j = 1;j< nums.length;j++){
+            if(nums[j]!=nums[i]){
+                nums[i+1]=nums[j];
+                i++;
+            }
+        }
+
+        System.out.println("Unique elemnts in array are:"+i+1);
+    }
+}
