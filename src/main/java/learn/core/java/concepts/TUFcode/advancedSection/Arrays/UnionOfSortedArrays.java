f6b06f31121efe4d0d88904f00bb4976f194def9
Sushant Wadjikar <wadjikarsushant@gmail.com>
2026-01-08 23:48:29 +0530

    Union of arrays

--- src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Arrays/UnionOfSortedArrays.java
+++ src/main/java/learn/core/java/concepts/TUFcode/advancedSection/Arrays/UnionOfSortedArrays.java
@@ -2,23 +2,20 @@ package learn.core.java.concepts.TUFcode.advancedSection.Arrays;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.stream.IntStream;
 
 public class UnionOfSortedArrays {
 
     public static void main(String[] args) {
         int[] num1 = {3, 4, 6, 7, 9, }, num2 = {1, 5, 7, 8, 8};
 
-        Set union= new HashSet<Integer>();
+       int[] unionArray = IntStream.concat(Arrays.stream(num1), Arrays.stream(num2))
+                .distinct() // Removes duplicates
+                .sorted()   // Ensures result is sorted
+                .toArray();
 
-        for(int i: num1)
-            union.add(i);
-
-        for(int i: num2)
-            union.add(i);
-
-        int[] intArray = union.stream().mapToInt(i -> (int) i).toArray();
-
-        Arrays.stream(intArray).forEach(System.out::print);
+        Arrays.stream(unionArray).forEach(System.out::print);
     }
 }
